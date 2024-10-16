7146b1aedee8312eebcefeb59a5a2eb10fde2a2d
Maryam Al-Naami <maryamalnaami@gmail.com>
2024-10-16 12:38:57 -0500

    Add test.cpp

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void print(string str);
+
+int main(){
+
+    string str = "testing ig";
+    print(str);
+
+    return 0;
+}
+
+void print(string s){
+
+    cout << "s" << endl;
+
+}
